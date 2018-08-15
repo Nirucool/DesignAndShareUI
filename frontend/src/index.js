1c5d7fc30db818e4bea4db842f0950b2ca18d870
Nirupama Sharma <nirupamasharma@Nirupamas-MacBook-Pro.local>
2018-08-15 10:32:13 -0400

    adding files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route} from 'react-router-dom';
-import '~bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import './App.css';
 import {Provider} from 'react-redux';
